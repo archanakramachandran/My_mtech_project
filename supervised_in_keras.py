84fad28e9a9beda47d5fea6b54ad973651e05704
archanakramachandran <95045891+archanakramachandran@users.noreply.github.com>
2023-07-06 16:18:44 +0530

    Update supervised_in_keras.py

--- supervised_in_keras.py
+++ supervised_in_keras.py
@@ -1,20 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 28 22:24:02 2023
-
-@author: hp
-"""
-
-#from scipy import misc,ndimage
-#from scipy.ndimage.interpolation import zoom
-#from scipy.misc import imread
-
-
-#from keras import backend as K
 from keras.utils.np_utils import to_categorical
-#from keras import layers
-#from keras.preprocessing.image import save_img
-#from keras.utils.vis_utils import model_to_dot
 from keras.applications.vgg16 import VGG16,preprocess_input
 from keras.applications.vgg16 import VGG16
 from keras.models import Sequential,Model
@@ -34,9 +18,12 @@ from imutils import paths
 import os
 import cv2   
 import numpy
+import tensorflow as tf
+import tensorflow_addons as tfa
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
 #%%
-
-
 train_path=r'I:\End_sem_major_project\data\train'
 
 num_classes = 25
@@ -44,8 +31,8 @@ input_shape = (256, 256,3)
 batch_size=32
 pathtoimages=list(paths.list_images(train_path))
 
-data=[] #x_train
-lables=[] #y_train
+x_train=[] #x_train
+y_train=[] #y_train
 lable=[] 
 for images in pathtoimages: #loop in image folder
   lable.clear()
@@ -54,9 +41,9 @@ for images in pathtoimages: #loop in image folder
   #  continue 
   image=cv2.imread(images)
   image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-  data.append(image)
-  lables.append(lable)
-print(lables)
+  x_train.append(image)
+  y_train.append(lable)
+#print(lables)
 
 #%%
 val_path= r'I:\End_sem_major_project\data\val' 
@@ -73,11 +60,12 @@ for images in test_pathtoimages:
   x_test.append(image)
   y_test.append(label)
 
-x_train=numpy.array(data)
-y_train=numpy.array(lables)
+x_train=numpy.array(x_train)
+y_train=numpy.array(y_train)
 x_test=numpy.array(x_test)
 y_test=numpy.array(y_test)
 
+print()
 print(f"x_train shape: {x_train.shape} - y_train shape: {y_train.shape}")
 print(f"x_test shape: {x_test.shape} - y_test shape: {y_test.shape}")
 
@@ -92,3 +80,98 @@ data_augmentation = keras.Sequential(
 )
 data_augmentation.layers[0].adapt(x_train)
 
+#%%
+def create_encoder():
+    resnet = keras.applications.ResNet50V2(
+        include_top=False, weights=None, input_shape=input_shape, pooling="avg"
+    )
+
+    inputs = keras.Input(shape=input_shape)
+    augmented = data_augmentation(inputs)
+    outputs = resnet(augmented)
+    model = keras.Model(inputs=inputs, outputs=outputs, name="cifar10-encoder")
+    return model
+
+
+encoder = create_encoder()
+encoder.summary()
+
+learning_rate = 0.001
+batch_size = 265
+hidden_units = 512
+projection_units = 128
+num_epochs = 50
+dropout_rate = 0.5
+temperature = 0.05
+
+#%%
+def create_classifier(encoder, trainable=True):
+
+    for layer in encoder.layers:
+        layer.trainable = trainable
+
+    inputs = keras.Input(shape=input_shape)
+    features = encoder(inputs)
+    features = layers.Dropout(dropout_rate)(features)
+    features = layers.Dense(hidden_units, activation="relu")(features)
+    features = layers.Dropout(dropout_rate)(features)
+    outputs = layers.Dense(num_classes, activation="softmax")(features)
+
+    model = keras.Model(inputs=inputs, outputs=outputs, name="cifar10-classifier")
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate),
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    )
+    return model
+#%%
+class SupervisedContrastiveLoss(keras.losses.Loss):
+    def __init__(self, temperature=1, name=None):
+        super().__init__(name=name)
+        self.temperature = temperature
+
+    def __call__(self, labels, feature_vectors, sample_weight=None):
+        # Normalize feature vectors
+        feature_vectors_normalized = tf.math.l2_normalize(feature_vectors, axis=1)
+        # Compute logits
+        logits = tf.divide(
+            tf.matmul(
+                feature_vectors_normalized, tf.transpose(feature_vectors_normalized)
+            ),
+            self.temperature,
+        )
+        return tfa.losses.npairs_loss(tf.squeeze(labels), logits)
+
+
+def add_projection_head(encoder):
+    inputs = keras.Input(shape=input_shape)
+    features = encoder(inputs)
+    outputs = layers.Dense(projection_units, activation="relu")(features)
+    model = keras.Model(
+        inputs=inputs, outputs=outputs, name="cifar-encoder_with_projection-head"
+    )
+    return model
+
+#%%
+encoder = create_encoder()
+
+encoder_with_projection_head = add_projection_head(encoder)
+encoder_with_projection_head.compile(
+    optimizer=keras.optimizers.Adam(learning_rate),
+    loss=SupervisedContrastiveLoss(temperature),
+)
+
+encoder_with_projection_head.summary()
+
+history = encoder_with_projection_head.fit(
+    x=x_train, y=y_train, batch_size=batch_size, epochs=num_epochs
+)
+
+#%%
+
+classifier = create_classifier(encoder, trainable=False)
+
+history = classifier.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=num_epochs)
+
+accuracy = classifier.evaluate(x_test, y_test)[1]
+print(f"Test accuracy: {round(accuracy * 100, 2)}%")
